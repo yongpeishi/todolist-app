6f1a7f621cec46edd8dc43be9a7aac7e2cc75b3a
yongpeishi <yongpeishi@gmail.com>
2015-04-13 11:19:27 +1000

    use props to pass data from parent to child component

--- src/main.js
+++ src/main.js
@@ -1,25 +1,9 @@
-var Task = React.createClass({
-  render: function() {
-    return (
-      React.DOM.div({className: 'task'},
-                    React.DOM.input({type: 'checkbox', name: 'task-completed', checked: this.props.isTaskCompleted}),
-                    React.DOM.span({}, this.props.children)
-                   )
-    );
-  }
-});
+// Pretend this come from the backend
+var data = [
+  {isCompleted: true,  text: "Sing a song"},
+  {isCompleted: false, text: "Dance"}
+];
 
-var TaskList = React.createClass({
-  render: function() {
-    return (
-      React.createElement('div', {className: 'taskList'},
-                          React.createElement(Task, {isTaskCompleted: true}, "Sing a song"),
-                          React.createElement(Task, {isTaskCompleted: false}, "Dance")
-        )
-
-    );
-  }
-});
 
 var TaskForm = React.createClass({
   render: function() {
@@ -32,12 +16,46 @@ var TaskForm = React.createClass({
 });
 
 
-var TaskBox = React.createClass({displayName: 'TaskBox',
+var Task = React.createClass({
+
+  updateTaskCompletion: function() {
+    // replace this to update the task
+    console.log('calling updateTaskCompletion');
+    return false;
+  },
+
+  render: function() {
+    return (
+      React.DOM.div({className: 'task'},
+                    React.DOM.input({type: 'checkbox',
+                                     name: 'task-completed',
+                                     checked: this.props.isTaskCompleted,
+                                     onChange: this.updateTaskCompletion
+                                     }),
+                    React.DOM.span({}, this.props.children)
+                   )
+    );
+  }
+});
+
+var TaskList = React.createClass({
+  render: function() {
+    var taskNodes = this.props.data.map(function(task) {
+      return React.createElement(Task, {isTaskCompleted: task.isCompleted}, task.text);
+    });
+
+    return React.createElement('div', {className: 'taskList'}, taskNodes);
+  }
+});
+
+var TaskBox = React.createClass({
+  displayName: 'TaskBox',
+
   render: function() {
     return (
       React.createElement('div', {className: "taskBox"},
         React.createElement('h1', null, "Tasks"),
-        React.createElement(TaskList, null),
+        React.createElement(TaskList, {data: data}),
         React.createElement(TaskForm, null)
 
       )
@@ -46,6 +64,6 @@ var TaskBox = React.createClass({displayName: 'TaskBox',
 });
 
 React.render(
-  React.createElement(TaskBox, null),
+  React.createElement(TaskBox, {data: data}),
   document.getElementById('container')
 );
