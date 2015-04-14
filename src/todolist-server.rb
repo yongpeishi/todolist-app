ecb60405090b2fd8b398652ea42c926481024212
yongpeishi <yongpeishi@gmail.com>
2015-04-14 14:52:35 +1000

    added route for adding new task

--- src/todolist-server.rb
+++ src/todolist-server.rb
@@ -8,10 +8,29 @@ get '/' do
 end
 
 get '/tasks.json' do
-  raw_json_data = File.read('tasks.json')
-  tasks = JSON.parse(raw_json_data)
-
   content_type :json
   cache_control :no_cache
+  tasks_from_file.to_json
+end
+
+post '/tasks' do
+  request.body.rewind
+  new_task = JSON.parse(request.body.read)
+  new_task['isCompleted'] = false
+  tasks = tasks_from_file
+  tasks << new_task
+  tasks_to_file( tasks )
+
+  content_type :json
   tasks.to_json
+end
+
+
+def tasks_from_file
+  raw_json_data = File.read('tasks.json')
+  JSON.parse(raw_json_data)
+end
+
+def tasks_to_file tasks
+  File.write('tasks.json', JSON.pretty_generate(tasks, :indent => '  '))
 end
